b54151ccd3727709f5dfa9cd8bc79e352b2c7624
Rishikesh Negi <rishinegi97@gmail.com>
2025-11-18 14:27:04 +0530

    Use custom hook to switch between desktop and mobile behavior

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,12 @@
+import { useActivateMobileLayout } from "../hooks/useActivateMobileLayout";
 import LeftSidebar from "./LeftSidebar";
 import MainContent from "./MainContent";
 import MobileSidebarOpeners from "./mobileSidebarOpeners";
 import RightSidebar from "./RightSidebar";
 
 function AppLayout() {
+  useActivateMobileLayout();
+
   return (
     <div
       className={`grid h-screen w-screen grid-rows-[7.8rem_1fr] grid-cols-[auto_1fr_36.4rem] text-text-primary overflow-hidden max-phone-lg:relative max-phone-lg:grid-cols-1`}>
