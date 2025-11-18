58869f8b21679d6fd5c7bf271d36a73a35949465
Rishikesh Negi <rishinegi97@gmail.com>
2025-11-18 12:57:53 +0530

    Fix vertical spacing in the 'Summary' tile

--- src/ui/ProjectSummaryRightSidebar.jsx
+++ src/ui/ProjectSummaryRightSidebar.jsx
@@ -2,7 +2,7 @@ import { formatDateAndTime } from "../helpers/formatDateAndTime";
 
 function ProjectSummaryRightSidebar({ project }) {
   return (
-    <div className="px-3 py-4 bg-background-1 rounded-lg flex flex-col gap-3 shadow-md/2 max-phone-lg:p-6 max-phone-lg:gap-10">
+    <div className="px-3 py-4 bg-background-1 rounded-lg flex flex-col gap-3 shadow-md/2 max-phone-lg:p-6 max-phone-lg:gap-6">
       <h4 className="text-2xl font-semibold max-phone-lg:text-4xl">Summary</h4>
       <span className="text-xl text-text-helper max-phone-lg:text-2xl">
         {formatDateAndTime(project?.date)}
